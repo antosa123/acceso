db05e879f3d805f77f1e78afa49737cc39685724
antosa123 <andrea_ets@hotmail.com>
2015-11-18 22:21:22 +0100

    18/11/15 solucionado

--- Streams_bytes/src/streambytes/StreamBytes.java
+++ Streams_bytes/src/streambytes/StreamBytes.java
@@ -6,10 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 public class StreamBytes {
 
 	static FileInputStream f1 = null;
 	static FileOutputStream f2 = null;
+	static String fichero, ficheroD;
 
 	public static void main(String[] args) {
 		//llamada al metodo para abrir el fichero
@@ -27,6 +30,8 @@ public class StreamBytes {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
 	
 	/**
@@ -43,9 +48,14 @@ public class StreamBytes {
 			while (byte_actual != -1) {
 				f2.write(byte_actual);
 				byte_actual = f1.read();
-
 			}
-
+			
+			File file = new File(fichero);
+			System.out.println(fichero+", "+file.length()+" Bytes");
+			
+			File file2 = new File(ficheroD);
+			System.out.println(ficheroD+", "+file.length()+" Bytes");
+				
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
@@ -59,14 +69,32 @@ public class StreamBytes {
 
 		boolean correcto = false;
 
-		while (correcto == false) {
+		while(correcto == false) {
 
 			try {
-
-				f1 = new FileInputStream("lena.jpg");
-				f2 = new FileOutputStream("fichero.jpg");
+				
+				fichero = JOptionPane.showInputDialog("Escribe el nombre del fichero de origen: ");
+				File f = new File(fichero);
+				
+				//ERROR le tengo que pasar la primera vez "lena.jpg"
+				//Le paso el fichero
+				f1 = new FileInputStream(f);
+				
+				ficheroD = JOptionPane.showInputDialog("Escribe el nombre del fichero de destino: ");
+				File fi = new File(ficheroD);
+				
+				if(fi.exists()){
+					//volver a preguntar
+					ficheroD = JOptionPane.showInputDialog("Escribe otro nombre para el fichero de destino: ");
+					fi = new File(ficheroD);
+				}
+									
+				
+				f2 = new FileOutputStream(fi);
+				
 				correcto = true;
 			} catch (Exception e) {
+				JOptionPane.getRootFrame().dispose();
 				System.err.println("Ha habido un error");
 			}
 		}
